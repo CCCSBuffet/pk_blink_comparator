d6a481c9837e620b1565e7a035f54103eded8090
Perry Kivolowitz <perry@kivolowitz.com>
2022-04-29 15:32:05 -0500

    updated blink

--- blink.py
+++ blink.py
@@ -1,5 +1,6 @@
 import cv2
 
+# Create an empty list and populate it with two pictures.
 images = []
 images.append(cv2.imread('p1.jpg'))
 images.append(cv2.imread('p2.jpg'))
@@ -7,7 +8,7 @@ current_index = 0
 key = ''
 while key != 'q':
     cv2.imshow('Pluto', images[current_index])
-    cv2.setWindowTitle('Pluto', str.format('Pluto Image: %d   -   \'q\' to quit' % current_index))
+    cv2.setWindowTitle('Pluto', 'Pluto Image: {:d}   -   \'q\' to quit'.format(current_index)))
     key = chr(cv2.waitKey(0))
     current_index = 1 - current_index
 cv2.destroyAllWindows()
